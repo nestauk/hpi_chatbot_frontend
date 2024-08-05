eab3a3a60be9854dbc74515fa3f31a2d2c16556d
sqr00t <797346+sqr00t@users.noreply.github.com>
2024-08-05 09:47:53 +0100

    feat(feedback, themes): add feedback feature and renbee theme

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import os
 from time import sleep
 from random import choice
-from typing import AsyncGenerator, Generator, Any, List
+from pathlib import Path
+from typing import AsyncGenerator, Generator, List
 
 import streamlit as st
 from streamlit_feedback import streamlit_feedback
+from streamlit_extras.stylable_container import stylable_container
 
 from langserve import RemoteRunnable
 
 from langfuse import Langfuse
 
 import asyncio
+
+from functools import partial
 
 TITLE = "PumpPal (Beta) - Heatpump Installer Assistant"
 MODEL_NAME = os.environ.get("MODEL_NAME")
@@ -53,26 +57,6 @@ def convo_starter_generator(
         yield word + " "
         sleep(chunk_delay)
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-    with st.chat_message("assistant"):
-        response = st.write_stream(
-            convo_starter_generator(
-                [
-                    "Hey! Pleasure to meet you, I'm PumpPal, your very own heat pump AI assistant. I'm currently in a Beta version, but I have information regarding installation manuals, a plumbing textbook and UK domestic heat pump best practice guidance. " + convo_starters[0]
-                ]
-            )
-        )
-        st.session_state.messages.append({"role": "assistant", "content": response})
-else:
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-            if "feedback" in message:
-                st.markdown(message["feedback"]["score"])
-
 async def achain_response_generator(
     query: str,
     chunk_delay: float = os.environ.get("CHUNK_DELAY", 0.05)
@@ -98,15 +82,19 @@ async def achain_response_generator(
             }
         },
         version="v1",
-        include_names=["StrOutputParser"],
+        include_names=["RunnableSequence"],
     ):
-        # Cache last generation run_id
-        if chunk["event"] == "on_start_start":
-            st.session_state.run_id = chunk["run_id"]
-
-        if chunk["event"] == "on_start_stream":
-            yield chunk["data"]["chunk"]
+        if (
+            (chunk["event"] == "on_chain_stream")
+            and ("chunk" in chunk["data"])
+            and ("answer" in chunk["data"]["chunk"])
+        ):
+            yield chunk["data"]["chunk"]["answer"]
             sleep(chunk_delay)
+
+        # Cache last generation run_id
+        if (chunk["event"] == "on_chain_start") and ("input" in chunk["data"]):
+            st.session_state.run_id = chunk["run_id"]
 
 def to_sync_generator(async_gen: AsyncGenerator):
     loop = asyncio.new_event_loop()
@@ -121,86 +109,197 @@ def to_sync_generator(async_gen: AsyncGenerator):
     finally:
         loop.close()
 
-# Accept user input
-if prompt := st.chat_input("Enter your query here..."):
-    # Add user message to chat history
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
+## Themed
+if "theme" in st.query_params:
+    bot = "Renbee"
+    themes_path = Path(__file__).parent / "themes"
+    with open(themes_path / f'{st.query_params["theme"]}.css') as f:
+        st.markdown('<style>' + f.read() + '</style>', unsafe_allow_html=True)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response = st.write_stream(
-            to_sync_generator(achain_response_generator(prompt))
-        )
-
-    with st.chat_message("assistant"):
-
-        run_id = st.session_state.run_id
-        st.write(run_id)
-        feedback_key = f"feedback_{run_id}"
-
-        def _on_submit(feedback):
-            # Define score mappings for both "thumbs" and "faces" feedback systems
-            score_mappings = {
-                "thumbs": {"👍": 1, "👎": 0},
-                "faces": {"😀": 1, "🙂": 0.75, "😐": 0.5, "🙁": 0.25, "😞": 0},
+    # Note: container key needs to be "somewhat" unique depending on purpose
+    bot_chat_container = partial(
+        stylable_container,
+        css_styles=[
+            """
+            .st-emotion-cache-1dgsum0 {
+                display: flex;
+                width: 2rem;
+                height: 2rem;
+                flex-shrink: 0;
+                border-radius: 0.5rem;
+                -webkit-box-align: center;
+                align-items: center;
+                -webkit-box-pack: center;
+                justify-content: center;
+                border: 1px solid rgba(20, 81, 100, 1);
+                background-color: rgba(20, 81, 100, 1);
+                color: rgb(255, 255, 255);
+                line-height: 1;
+                font-size: 1rem;
             }
+            """,
+            """
+            div[data-testid="stChatMessage"] {
+                background-color: rgba(240, 242, 246, 0.5);
+            }
+            """,
+        ]
+    )
 
-            # Get the score mapping based on the selected feedback option
-            scores = score_mappings["faces"]
+    user_chat_container = partial(
+        stylable_container,
+        css_styles=[
+            """
+            .st-emotion-cache-1dgsum0 {
+                display: flex;
+                width: 2rem;
+                height: 2rem;
+                flex-shrink: 0;
+                border-radius: 0.5rem;
+                -webkit-box-align: center;
+                align-items: center;
+                -webkit-box-pack: center;
+                justify-content: center;
+                border: 1px solid gray;
+                background-color: gray;
+                color: rgb(255, 255, 255);
+                line-height: 1;
+                font-size: 1rem;
+            }
+            """,
+            """
+            div[data-testid="stChatMessage"] {
+                background-color: rgb(255, 255, 255);
+                opacity: 0.4;
+            }
+            """,
+        ]
+    )
 
-            if not feedback:
-                raise Exception("feedback isn't working")
-            else:
-                # Get the score from the selected feedback option's score mapping
-                score = scores.get(feedback["score"])
-                comment = feedback.get("text")
-
-                if score is not None:
-                    langfuse_client.score(
-                        trace_id=run_id,
-                        name="user_feedback",
-                        data_type="NUMERIC",
-                        score=score,
-                        comment=comment,
+    # Initialize chat history
+    # Purpose of styled containers here is to contain the intro message generation
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        with bot_chat_container(key="intro_bot_msg"):
+            with st.chat_message(bot):
+                response = st.write_stream(
+                    convo_starter_generator(
+                        [
+                            "Hey! Pleasure to meet you, I'm PumpPal, your very own heat pump AI assistant. I'm currently in a Beta version, but I have information regarding installation manuals, a plumbing textbook and UK domestic heat pump best practice guidance. " + convo_starters[0]
+                        ]
                     )
+                )
 
-                    st.session_state.feedback = {
-                        "feedback_id": feedback_key,
-                        "score": score,
-                    }
-                else:
-                    st.warning("Invalid feedback score.")
-            return {
-                "score": score,
-                "feedback": comment,
-            }
-                
+                st.session_state.messages.append({"role": "assistant", "content": response})
 
-        feedback = streamlit_feedback(
-            feedback_type="faces",
-            optional_text_label="[Optional] Please provide an explanation",
-            key=feedback_key,
-            on_submit=_on_submit
-        )
+    # Purpose here is to contain historical messages that are unique
+    # to the incrementing order that they were appended to in session_state
+    else:
+        message_id = 0
+        for message in st.session_state.messages:
+            if message["role"] == "assistant":
+                with bot_chat_container(key=f"bot_msg_{message_id}"):
+                    with st.chat_message(name=bot):
+                        st.markdown(message["content"])
 
+            else:
+                with user_chat_container(key=f"user_msg_{message_id}"):
+                    with st.chat_message(name="user", avatar=":material/engineering:"):
+                        st.markdown(message["content"])
+            message_id += 1
 
-        mock_feedback = {
-            "score": 3,
-            "feedback": "testing"
-        }
-        
-        langfuse_client.score(
-            trace_id=run_id,
-            name="user_feedback",
-            data_type="NUMERIC",
-            score=score,
-            comment=comment,
-        )
+    # Purpose of styled containers here is to print the markdown of a new user input
+    # and stream the new LLM-chain response. These will be overwritten by newer messages.
+    # Accept user input
+    if prompt := st.chat_input("Enter your query here..."):
+        with user_chat_container(key=f"user_msg_new"):
+            with st.chat_message("user", avatar=":material/engineering:"):
+                st.markdown(prompt)
 
-        feedback=mock_feedback
+                # Add user message to chat history
+                st.session_state.messages.append({"role": "user", "content": prompt})
 
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response, "feedback": feedback})
+        # Display assistant response in chat message container
+        with bot_chat_container(key=f"bot_msg_new"):
+            with st.chat_message(bot):
+                response = st.write_stream(
+                    to_sync_generator(achain_response_generator(prompt))
+                )
+
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": response})
+
+                if "feedback" not in st.session_state:
+                    st.session_state["feedback"] = None
+        with stylable_container(key="feedback_container", css_styles=[""]):
+            feedback = streamlit_feedback(
+                feedback_type="faces",
+                optional_text_label="[Optional] Please provide an explanation",
+                key="feedback",
+            )
+
+## Default app
+else:
+    bot = "assistant"
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        with st.chat_message(bot):
+                response = st.write_stream(
+                    convo_starter_generator(
+                        [
+                            "Hey! Pleasure to meet you, I'm PumpPal, your very own heat pump AI assistant. I'm currently in a Beta version, but I have information regarding installation manuals, a plumbing textbook and UK domestic heat pump best practice guidance. " + convo_starters[0]
+                        ]
+                    )
+                )
+
+                st.session_state.messages.append({"role": "assistant", "content": response})
+    # Display chat messages from history on app rerun
+    else:
+        for message in st.session_state.messages:
+            if message["role"] == "assistant":
+                with st.chat_message(name=bot):
+                    st.markdown(message["content"])
+            else:
+                with st.chat_message(name="user", avatar=":material/engineering:"):
+                    st.markdown(message["content"])
+
+    # Accept user input
+    if prompt := st.chat_input("Enter your query here..."):
+        with st.chat_message("user", avatar=":material/engineering:"):
+            st.markdown(prompt)
+
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # Display assistant response in chat message container
+        with st.chat_message(bot):
+            response = st.write_stream(
+                to_sync_generator(achain_response_generator(prompt))
+            )
+
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+            if "feedback" not in st.session_state:
+                st.session_state["feedback"] = None
+        with stylable_container(key="feedback_container", css_styles=[""]):
+            feedback = streamlit_feedback(
+                feedback_type="faces",
+                optional_text_label="[Optional] Please provide an explanation",
+                key="feedback",
+            )
+
+# add user feedback as a score
+if ("feedback" in st.session_state) and (st.session_state["feedback"] is not None):
+    emoji = st.session_state["feedback"]["score"]
+    scores_mapping = {"😀": 5, "🙂": 4, "😐": 3, "🙁": 2, "😞": 1}
+    score = scores_mapping.get(emoji)
+    langfuse_client.score(
+        trace_id=st.session_state["run_id"],
+        name="user_feedback",
+        data_type="NUMERIC",
+        value=score,
+        comment=f'{emoji} {st.session_state["feedback"]["text"]}',
+    )
